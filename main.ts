e0c0c5cef642a35527d45658483622b1431162b0
Kopi-Kopsen <posnerla@thg-heilbronn.de>
2024-10-09 10:03:01 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
 let Feuchtigkeit = 0
 basic.forever(function () {
-    Feuchtigkeit = Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, 0, 100)
+    Feuchtigkeit = pins.analogReadPin(AnalogPin.P1)
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
         basic.showNumber(Feuchtigkeit)
     }
 })
 basic.forever(function () {
-    if (Feuchtigkeit >= 10) {
+    if (Feuchtigkeit <= 8) {
+        basic.setLedColor(0x0000ff)
+    } else if (Feuchtigkeit >= 100) {
         basic.setLedColor(0xff0000)
     } else {
         basic.setLedColor(0x00ff00)
